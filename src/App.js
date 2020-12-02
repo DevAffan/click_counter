59abd3beb3a2315a8daa71877cc3b13586543a37
Affan Ali <affanali577@gmail.com>
2020-12-03 00:45:04 +0500

    counter update with day night button

--- src/App.js
+++ src/App.js
@@ -5,15 +5,30 @@ import './App.css';
 function App() {
 
 let [count, setCount] = useState(0)
+let [day , setDay] = useState(false)
   return (
-    
-    <div class='c_body'>
+    <div className= { ` c_day ${ day ? 'day'  : ' '} ` }>
       <h3>Here is the counter: {count}</h3>
       <button onClick={
         ()=> setCount(count + 1)}>
           Click to React
       </button>
-      <p class='auth'>Created by: AFFAN ALI</p>
+        <br/>
+        <br/>
+        <br/>
+        <hr/>
+
+      <h2>Good {day ? 'Morning' : 'Night'}</h2>
+     
+      <button onClick={
+        ()=>setDay(!day)
+      }>
+        Time
+      </button>
+
+      <p className='auth'>Created by: AFFAN ALI</p>
+
+      
     </div>
   );
 }
